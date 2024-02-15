bc81647b38d6dc9141371e26377bbcdbb2ed7958
isay83 <jisay.2003@gmail.com>
2024-02-15 10:52:53 -0600

    add p152 advance

---
+++ p152.php
@@ -0,0 +1,24 @@
+<?php
+
+// Jonathan Isay Bernal Arriaga
+// 2024 Ene-Jun
+
+$cases = trim(fgets(STDIN));
+
+for ($iterate=0; $iterate<$cases; $iterate++){
+    $process = trim(fgets(STDIN));
+    $jar_proc = explode(' ', $process);
+}
+
+print_r($jar_proc);
+
+function jar_filling(){
+    $filled = 0;
+    $extra = 0;
+
+    for ($iterate=1; $iterate<=$jar_proc[0]; $iterate++) {
+
+    }
+}
+
+?>
