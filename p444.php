777ec5f390772541659d1b39db46d926a7e8c75e
isay83 <jisay.2003@gmail.com>
2024-04-04 21:02:26 -0600

    add first logical code

--- p444.php
+++ p444.php
@@ -1,2 +1,22 @@
+<?php
 // Jonathan Isay Bernal Arriaga
 // 2024 Ene-Jun
+
+// Here is your key: 88d5934e
+$queries = trim(fgets(STDIN));
+getData($queries);
+
+function getData($queries)
+{
+    for ($i = 0; $i < $queries; $i++) {
+        $entry = trim(fgets(STDIN));
+        list($search, $text) = explode(' ', $entry, 2);
+
+        print_r($search);
+        print_r($text);
+    }
+}
+
+function searchMovie($search, $text)
+{
+}
