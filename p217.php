c4ba2d4eb4bacda855c01219e096ca028b293454
isay83 <jisay.2003@gmail.com>
2024-04-09 14:33:23 -0600

    add p217 file

--- p217.php
+++ p217.php
@@ -1,1 +1,35 @@
 <?php
+// Jonathan Isay Bernal Arriaga
+// 2024 Ene-Jun
+
+$server = trim(fgets(STDIN));
+$user = trim(fgets(STDIN));
+$password = trim(fgets(STDIN));
+$dataBase = trim(fgets(STDIN));
+
+// Create connection
+$connection = mysqli_connect($server, $user, $password, $dataBase);
+// Check connection
+if ($connection) {
+    // Query to determinate winner
+    $query = "SELECT CONCAT(u.Nombre, ' ', u.Apellidos) AS usuario, SUM(j.puntos) AS puntos
+    FROM usuarios u
+    JOIN (
+        SELECT id_usuario AS usuario, puntos FROM juegos WHERE ganador = id_usuario
+        UNION ALL
+        SELECT id_invitado AS usuario, puntos FROM juegos WHERE ganador = id_invitado
+    ) j ON u.Usuario = j.usuario
+    GROUP BY 1
+    ORDER BY puntos DESC
+    LIMIT 1;
+    ";
+    // Get result
+    $result = mysqli_query($connection, $query);
+    // Print data
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        echo $row["usuario"] . " " . $row["puntos"];
+    }
+    // Close connection
+    mysqli_close($connection);
+}
